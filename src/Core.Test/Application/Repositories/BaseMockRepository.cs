a7d2e0e7a8e7b8158a986d0d0ae9bbdedfcbbda4
acan25 <alpercan2012@gmail.com>
2026-04-16 00:15:18 +0300

    refactor: improve formatting and add NullLoggerFactory to MapperConfiguration

--- src/Core.Test/Application/Repositories/BaseMockRepository.cs
+++ src/Core.Test/Application/Repositories/BaseMockRepository.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
 using NArchitecture.Core.Application.Rules;
 using NArchitecture.Core.Localization.Resource.Yaml;
@@ -22,10 +23,13 @@ public abstract class BaseMockRepository<TRepository, TEntity, TEntityId, TMappi
     public BaseMockRepository(TFakeData fakeData)
     {
         MapperConfiguration mapperConfig =
-            new(c =>
-            {
-                c.AddProfile<TMappingProfile>();
-            });
+            new(
+                c =>
+                {
+                    c.AddProfile<TMappingProfile>();
+                },
+                NullLoggerFactory.Instance
+            );
         Mapper = mapperConfig.CreateMapper();
 
         MockRepository = MockRepositoryHelper.GetRepository<TRepository, TEntity, TEntityId>(fakeData.Data);
